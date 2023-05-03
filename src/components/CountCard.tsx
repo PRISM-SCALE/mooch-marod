6ed9bb770792939c6f21710f23a77b4f3b88814a
CodeRAFCE <seshan.io@prismscale.com>
2023-05-03 13:39:24 +0530

    added count animation when in view

--- src/components/CountCard.tsx
+++ src/components/CountCard.tsx
@@ -1,4 +1,5 @@
 import {Box, Typography} from "@mui/material";
+import {useInView} from "react-intersection-observer";
 import CountUp from "react-countup";
 
 type Props = {
@@ -7,16 +8,21 @@ type Props = {
 };
 
 const CountCard = ({count, text}: Props) => {
+	const [ref, inView] = useInView({
+		threshold: 0.3,
+		triggerOnce: true,
+	});
 	return (
 		<Box
 			sx={{
 				textAlign: "center",
 				mt: {xs: "1rem", md: 0},
 			}}
+			ref={ref}
 		>
 			{/* Component for counts+ */}
 			<Typography component="span" variant="h1" fontWeight={600}>
-				<CountUp duration={5} end={count} />+
+				<CountUp duration={5} end={inView ? count : 0} useEasing={true} />+
 			</Typography>
 			<br />
 			<Typography component="span" sx={{fontSize: "1.2rem"}}>
