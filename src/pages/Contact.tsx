30ae8e7d91e1e83c2b9f3502a005d561a1d364b1
CodeRAFCE <seshan.io@prismscale.com>
2023-05-27 17:37:16 +0530

    PageHeader in contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,9 +1,11 @@
 import {Box} from "@mui/material";
 import ContactUs from "../sections/Contact/ContactUs";
+import PageHeader from "../sections/PageHeader";
 
 const Contact = () => {
 	return (
 		<Box>
+			<PageHeader tagline="connect" title="contact us" />
 			<ContactUs />
 		</Box>
 	);
