fe86b34fc06dce3561a5e85e4362a9d05eec59c7
CodeRAFCE <seshan.io@prismscale.com>
2023-06-21 02:36:08 +0530

    adding logos

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -4,6 +4,9 @@ export const mooch_icon = "/mooch.svg";
 export const logo_white = "/images/fk_white.png";
 export const logo_black = "/images/fk_black.png";
 
+export const logo_white_mm = "/images/mm_logo_white.png";
+export const logo_black_mm = "/images/mm_logo_black.png";
+
 export const fk_background =
 	"linear-gradient(359.53deg, #000000 0.43%, #242424 53.13%, #000000 96.53%), url(/images/brick_wall.png)";
 
