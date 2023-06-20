977255a1963759c009fd04d3dcf21914c4976e0a
CodeRAFCE <seshan.io@prismscale.com>
2023-06-21 01:32:03 +0530

    deleting old outlets

--- src/pages/Outlets.tsx
+++ src/pages/Outlets.tsx
@@ -1,21 +1,12 @@
 import {Box} from "@mui/material";
 
-//HOOKS
-import {useResponsive} from "../hooks/useResponsive";
-
 // SECTIONS
 import PageHeader from "../sections/PageHeader";
-import DesktopAddress from "../sections/Outlets/DesktopAddress";
-import MobileAddress from "../sections/Outlets/MobileAddress";
 
 const Outlets = () => {
-	const {largeScreenAndUp, isSmallScreen, isMediumScreen, isLargeScreen} = useResponsive();
 	return (
 		<Box>
 			<PageHeader tagline="Find Us" title="Outlets" />
-			{largeScreenAndUp ? <DesktopAddress /> : null}
-
-			{isSmallScreen || isMediumScreen || isLargeScreen ? <MobileAddress /> : null}
 		</Box>
 	);
 };
