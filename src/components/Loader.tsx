b1ba3aadbd39262187fbc43c255da7d2eced4849
CodeRAFCE <seshan.io@prismscale.com>
2023-06-21 00:25:28 +0530

    added loader component

---
+++ src/components/Loader.tsx
@@ -0,0 +1,30 @@
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
+
+import {mm_background} from "../utils/common";
+
+export default function Loader() {
+	// const [open, setOpen] = React.useState(false);
+	// const handleClose = () => {
+	// 	setOpen(false);
+	// };
+	// const handleOpen = () => {
+	// 	setOpen(true);
+	// };
+
+	return (
+		<div>
+			<Backdrop
+				sx={{
+					color: "#fff",
+					zIndex: (theme) => theme.zIndex.drawer + 1,
+					background: mm_background,
+				}}
+				open={true}
+				// onClick={handleClose}
+			>
+				<CircularProgress color="inherit" />
+			</Backdrop>
+		</div>
+	);
+}
