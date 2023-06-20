8d43310ecf144b1115502b5df9d676603a985995
CodeRAFCE <seshan.io@prismscale.com>
2023-06-21 01:48:14 +0530

    updated menu

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,12 +1,12 @@
 import {Box} from "@mui/material";
 import PageHeader from "../sections/PageHeader";
-import MenuDasboard from "../sections/Menu/MenuDasboard";
+import MenuList from "../sections/Menu/MenuList";
 
 const Menu = () => {
 	return (
 		<Box>
-			<PageHeader tagline="Choose your menu" title="RESTAURANT MENU" />
-			<MenuDasboard />
+			<PageHeader tagline="Choose your delicacy" title="JABARDAST MENU" />
+			<MenuList />
 		</Box>
 	);
 };
